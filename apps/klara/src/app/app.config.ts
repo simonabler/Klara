384747e835608230d32676a82c07b6a56b6b592e
Simon Abler <simon.abler@ematric.com>
2026-05-14 14:39:16 +0200

    feat: register Locale

--- apps/klara/src/app/app.config.ts
+++ apps/klara/src/app/app.config.ts
@@ -1,8 +1,11 @@
 import {
   ApplicationConfig,
+  LOCALE_ID,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeDeAt from '@angular/common/locales/de-AT';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -12,6 +15,8 @@ import {
 } from '@angular/platform-browser';
 import { authInterceptor } from './auth/auth.interceptor';
 
+registerLocaleData(localeDeAt);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
@@ -20,5 +25,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    { provide: LOCALE_ID, useValue: 'de-AT' },
   ],
 };
